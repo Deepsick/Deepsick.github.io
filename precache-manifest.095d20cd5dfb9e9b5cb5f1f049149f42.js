3fe4a70e76943f0630a4e11f5a1cb3c09401bb2a
deepsick <mav96@bk.ru>
2019-04-18 15:55:11 +0300

    refreshes build

---
+++ precache-manifest.095d20cd5dfb9e9b5cb5f1f049149f42.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d81df4d4edb734d9713d8ffbd4374204",
+    "url": "/static/media/previous-page-icon.d81df4d4.svg"
+  },
+  {
+    "revision": "b767ab034ea02ad3322e6073eab05426",
+    "url": "/static/media/first-page-icon.b767ab03.svg"
+  },
+  {
+    "revision": "6b56b616ee9420fbb9fe38733d3d8a48",
+    "url": "/static/media/back-button-hover.6b56b616.svg"
+  },
+  {
+    "revision": "6858fa2df4c53f5f87f9",
+    "url": "/static/js/runtime~main.2e2cfd8a.js"
+  },
+  {
+    "revision": "587617aee570431fb4e9",
+    "url": "/static/js/main.ce4167ac.chunk.js"
+  },
+  {
+    "revision": "356daf71f9f24ad7d4ab",
+    "url": "/static/js/4.6f08c3cd.chunk.js"
+  },
+  {
+    "revision": "77c0cf3b50928fa1ca26",
+    "url": "/static/js/3.2f55ba38.chunk.js"
+  },
+  {
+    "revision": "13cecd05eabb35c516b1",
+    "url": "/static/js/2.8306fbc2.chunk.js"
+  },
+  {
+    "revision": "587617aee570431fb4e9",
+    "url": "/static/css/main.460c915c.chunk.css"
+  },
+  {
+    "revision": "913eee41c9575e9700a157eee5483ae2",
+    "url": "/index.html"
+  }
+];
