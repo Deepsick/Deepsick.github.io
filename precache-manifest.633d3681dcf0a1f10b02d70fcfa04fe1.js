510a20311e1b9eb2a7676c86094b3c394150ca36
deepsick <mav96@bk.ru>
2019-04-18 15:52:28 +0300

    adds build

---
+++ precache-manifest.633d3681dcf0a1f10b02d70fcfa04fe1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d81df4d4edb734d9713d8ffbd4374204",
+    "url": "/tv-shows-app/static/media/previous-page-icon.d81df4d4.svg"
+  },
+  {
+    "revision": "b767ab034ea02ad3322e6073eab05426",
+    "url": "/tv-shows-app/static/media/first-page-icon.b767ab03.svg"
+  },
+  {
+    "revision": "6b56b616ee9420fbb9fe38733d3d8a48",
+    "url": "/tv-shows-app/static/media/back-button-hover.6b56b616.svg"
+  },
+  {
+    "revision": "f7d615f6762c02d030d1",
+    "url": "/tv-shows-app/static/js/runtime~main.fbfdf222.js"
+  },
+  {
+    "revision": "9117a2487a1656525a01",
+    "url": "/tv-shows-app/static/js/main.a9a9c170.chunk.js"
+  },
+  {
+    "revision": "356daf71f9f24ad7d4ab",
+    "url": "/tv-shows-app/static/js/4.6f08c3cd.chunk.js"
+  },
+  {
+    "revision": "589979012f122b0e80d5",
+    "url": "/tv-shows-app/static/js/3.3c1716cd.chunk.js"
+  },
+  {
+    "revision": "13cecd05eabb35c516b1",
+    "url": "/tv-shows-app/static/js/2.8306fbc2.chunk.js"
+  },
+  {
+    "revision": "9117a2487a1656525a01",
+    "url": "/tv-shows-app/static/css/main.04795051.chunk.css"
+  },
+  {
+    "revision": "cf31f3ec246c85d7902cd2e27f6f8115",
+    "url": "/tv-shows-app/index.html"
+  }
+];
